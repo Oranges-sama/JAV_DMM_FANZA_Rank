4b854909d003b355a7312dc362f9bd96b32182a3
orange jam <114939349+Oranges-sama@users.noreply.github.com>
2023-10-07 00:22:13 +0800

    Add files via upload

--- top100.py
+++ top100.py
@@ -2,28 +2,39 @@
 import requests
 import re
 
-# 定义网站的url
-url = "https://github.com/Oranges-sama/JAV_DMM_FANZA_Rank/blob/main/original.php"
+# 定义文件的路径
+path = "original.php"
 
-# 发送get请求，获取网页内容
-response = requests.get(url)
+# 定义正则表达式，匹配序号和名字
+pattern = r"<span class=\"rank\">(\d+)</span>\s*<a href=\"[^\"]+\"><img src=\"[^\"]+\" alt=\"([^\"]+)\"></a>"
 
-# 检查响应状态码
-if response.status_code == 200:
-    # 获取网页文本
-    text = response.text
-    # 定义正则表达式，匹配序号和名字
-    pattern = r"<span class=\"rank\">(\d+)</span>\n<a href=\".*?\">(.*?)</a>"
+# 使用try...except语句来捕获并处理可能发生的异常
+try:
+    # 打开文件，读取内容
+    with open(path, "r", encoding="utf-8") as f:
+        response = f.read()
+
     # 使用re.findall方法，找出所有符合条件的结果
-    results = re.findall(pattern, text)
-    # 打开一个文件，用于写入结果
-    with open("top100.txt", "w", encoding="utf-8") as f:
-        # 遍历结果列表
-        for result in results:
-            # 将序号和名字用逗号分隔，写入文件中
-            f.write(",".join(result) + "\n")
-    # 打印完成信息
-    print("Done!")
-else:
-    # 打印错误信息
-    print("Error:", response.status_code)
+    results = re.findall(pattern, response)
+
+    # 判断是否有匹配结果
+    if results:
+        # 打开一个文件，用于写入结果
+        with open("top100.txt", "w", encoding="utf-8") as f:
+            # 遍历结果列表
+            for result in results:
+                # 将序号和名字用-号分隔，写入文件中
+                f.write("-".join(result) + "\n")
+        # 打印完成信息
+        print("Done!")
+        # 读取文件内容
+        with open("top100.txt", "r", encoding="utf-8") as f:
+            content = f.read()
+            # 打印文件内容
+            print(content)
+    else:
+        # 打印没有匹配结果的信息
+        print("No matches found.")
+except Exception as e:
+    # 打印异常信息
+    print("An error occurred:", e)
